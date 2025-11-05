28baa9807245be2c19f0ade4ba27f381ab482bdd
Guilherme Giannini <guilhermegianninimelo@gmail.com>
2025-11-04 21:43:56 -0300

    Adicionando services

--- Repositories/ICategoryRepository.cs
+++ Repositories/ICategoryRepository.cs
@@ -5,10 +5,10 @@ namespace VShop.ProductApi.Repositories
     public interface ICategoryRepository
     {
         Task<IEnumerable<Category>> GetAll();
-        Task<IEnumerable<Category>> GetCategoriesWithProducts();
+        Task<IEnumerable<Category>> GetCategoriesProducts();
         Task<Category?> GetById(int id);
         Task<Category> Create(Category category);
-        Task<Category?> Update(int id, Category category);
+        Task<Category?> Update(Category category);
         Task<Category> Delete(int id);
     }
 }
---
+++ Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Models;
+using VShop.ProductApi.Repositories;
+
+namespace VShop.ProductApi.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IMapper _mapper;
+        private IProductRepository _productRepository;
+
+        public ProductService(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+        public async Task AddProduct(ProductDTO productDto)
+        {
+            var productEntity = _mapper.Map<Product>(productDto);
+            await _productRepository.Create(productEntity);
+            productDto.Id = productEntity.Id;
+        }
+
+        public async Task<ProductDTO> GetProductById(int id)
+        {
+            var productEntity = await _productRepository.GetById(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _productRepository.GetAll();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public Task RemoveProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateProduct(ProductDTO productDto)
+        {
+            var categoryEntity = _mapper.Map<Product>(productDto);
+            await _productRepository.Update(categoryEntity);
+        }
+    }
+}
